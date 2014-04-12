d4b8d30bcdc955d9cad63ffd0eeec7e666b9969d
Sarah Gray <sgray@neat.com>
2014-04-11 22:07:44 -0400

    updating specs to reflect parent pages being reported for busted links

--- simple_spider_test.py
+++ simple_spider_test.py
@@ -5,43 +5,44 @@ from simple_spider import *
 
 
 class TestSpider(unittest.TestCase):
+
 	def setUp(self):
 		self.page = open("test_files/sample_page.html", "r").read()
 		self.session = requests.Session()
 		self.mock = mox.Mox()
 		self.mock.StubOutWithMock(requests, 'get')
-		
+
 	def tearDown(self):
 		self.mock.UnsetStubs()
-		
+
 	def test_get_alL_pages_for_domain(self):
 		sitemap_xml = open("test_files/sitemap.xml","r").read()
 		self.session.mount('http://www.devlogged.com', TestAdapter(sitemap_xml, status=200))
 		requests.get(mox.IgnoreArg()).AndReturn(self.session.get('http://www.devlogged.com'))
 		self.mock.ReplayAll()
-		
+
 		links = ['http://www.devlogged.com/about/', 'http://www.devlogged.com/tools/']
-		self.assertEqual(links, get_alL_pages_for_domain('http://www.devlogged.com'))
-	
+		self.assertEqual(links, get_all_pages_for_domain('http://www.devlogged.com'))
+
 	def test_extract_links_from_html(self):
 		self.session.mount('http://www.devlogged.com/about', TestAdapter(self.page, status=200))
 		requests.get(mox.IgnoreArg()).AndReturn(self.session.get('http://www.devlogged.com/about'))
 		self.mock.ReplayAll()
-		
-		links = {'http://wordpress.com' : 'found', 'http://aws.amazon.com/ec2/' : 'found'}
+
+		links = {'http://wordpress.com' : 'http://www.devlogged.com/about', 'http://aws.amazon.com/ec2/' : 'http://www.devlogged.com/about'}
 		extracted_links = extract_links_from_html('http://www.devlogged.com/about')
 		self.assertEqual(links, extracted_links)
 		self.assertEqual(2, len(extracted_links))
-		
+
 	def test_inspect_links(self):
 		self.session.mount('http://wordpress.com', TestAdapter(self.page, status=200))
 		self.session.mount('http://aws.amazon.com/ec2/', TestAdapter(self.page, status=404))
 		requests.get('http://wordpress.com').AndReturn(self.session.get('http://wordpress.com'))
 		requests.get('http://aws.amazon.com/ec2/').AndReturn(self.session.get('http://aws.amazon.com/ec2/'))
 		self.mock.ReplayAll()
-		
+
 		urls = {'http://wordpress.com' : 'found', 'http://aws.amazon.com/ec2/' : 'found'}
-		bad_links = {'http://aws.amazon.com/ec2/' : '404'}
+		bad_links = {'http://aws.amazon.com/ec2/' : {'status': '404', 'parent_page': 'found'}}
 		self.assertEquals(bad_links, inspect_links(urls))
-			
+
 unittest.main()
